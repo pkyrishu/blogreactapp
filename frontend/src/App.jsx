e321f6d1a63687f7855311a42ab2541ebe140e9c
praveen yadav <pky20026@gmail.com>
2026-06-17 14:27:20 +0530

    inesial bilid

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <div>AI Blogs</div>
+      {/*create a list of ai blofs*/}
+      <ul>
+        <li>AI in Healthcare</li>
+        <li>AI in Education</li>
+        <li>AI in Finance</li>
+        <li>AI in Transportation</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
