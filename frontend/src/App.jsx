672fa144e73db35498c2368e6295a5950f64b722
praveen yadav <pky20026@gmail.com>
2026-06-17 15:13:25 +0530

    new changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,6 +11,16 @@ const App = () => {
         <li>AI in Finance</li>
         <li>AI in Transportation</li>
       </ul>
+      //cleate login form
+      <form>
+        <label htmlFor="username">Username:</label>
+        <input type="text" id="username" name="username" />
+        <br />
+        <label htmlFor="password">Password:</label>
+        <input type="password" id="password" name="password" />
+        <br />
+        <button type="submit">Login</button>
+      </form>
     </>
   )
 }
